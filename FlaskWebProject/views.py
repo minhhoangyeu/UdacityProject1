58dde71c1fc1707217c1bcce4243b81ad9f7a444
minhhoangyeu <39401152+minhhoangyeu@users.noreply.github.com>
2024-06-10 16:42:55 +0700

    Update views.py
    t

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -13,8 +13,8 @@ from FlaskWebProject.models import User, Post
 import msal
 import uuid
 
-#imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER']  + '/'
-imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']+ '/' + app.config['BLOB_CONTAINER']  + '/'
+imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER']  + '/'
+#imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']+ '/' + app.config['BLOB_CONTAINER']  + '/'
 @app.route('/')
 @app.route('/home')
 @login_required
